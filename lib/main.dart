b5a879dfddea794ad494cfb8c743eff78581ae06
wonjaeyeon <woncow977@gmail.com>
2024-08-18 18:51:27 +0900

    testing if the push successes when there is no error-v5

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // checking the CI/CD pipeline
 
-// cheking now when there is no error -v4
+// cheking now when there is no error -v5
 
 void main() {
   runApp(const MyApp());
